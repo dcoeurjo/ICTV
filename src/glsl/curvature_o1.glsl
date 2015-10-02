15ed8ac119824660fa6dffdce467c86d4b75c061
Helene <perrier.ln@gmail.com>
2015-10-02 21:30:43 +0200

    Vertex shader crache courbure + direction, qui sont ensuite reprises par le GS et colorées dans le FS Ca permettra de faire cracher toutes ces infos au VS dans des transform feedback et de faire le calcul en deux temps. un premier temps qui calcule les valeurs et sort un min/max et un second temps qui shade l'objet et genere la geom en fonction ca devrait permettre aussi d'exporter les valeurs par la suite.

--- src/glsl/curvature_o1.glsl
+++ src/glsl/curvature_o1.glsl
@@ -5,51 +5,16 @@
 in vec4 position;
 
 uniform float u_curv_radius;
-uniform float u_kmin;
-uniform float u_kmax;
-uniform int u_ground_truth;
 
 uniform sampler3D u_xyz2_tex;
 uniform sampler3D u_xy_yz_xz_tex;
 
 out vec3 vertex_position;
 out vec3 vertex_color;
+
+out float curv_value;
 out vec3 curv_dir_min;
 out vec3 curv_dir_max;
-
-vec3 HSVtoRGB(vec3 hsv)
-{
-  int i;
-  double f, p, q, t;
-  if( hsv.y == 0 ) {                     // achromatic (gray)
-    return vec3(hsv.z);
-  }
-  i = int( floor( hsv.x / 60 ) );
-  f = ( hsv.x / 60 ) - i;                        // factorial part of h
-  p = hsv.z * ( 1.0 - hsv.y );
-  q = hsv.z * ( 1.0 - hsv.y * f );
-  t = hsv.z * ( 1.0 - hsv.y * ( 1.0 - f ) );
-
-  if (i==0)
-  	return vec3(hsv.z, t, p);
-  if (i==1)
-  	return vec3(q, hsv.z, p);
-  if (i==2)
-  	return vec3(p, hsv.z, t);
-  if (i==3)
-  	return vec3(p, q, hsv.z);
-  if (i==4)
-  	return vec3(t, p, hsv.z);
-
-  return vec3(hsv.z, p, q);
-}
-
-vec3 colormap(float scale)
-{
-  float cycles = 1;
-  const double hue = 360 * ( scale * cycles - floor(scale * cycles));
-  return HSVtoRGB( vec3(hue, 0.9, 1.0) );
-}
 
 void main( )
 {
@@ -74,15 +39,7 @@ void main( )
 	
 	float curvature = fact83r - fact4pir4*volume;
 
-	gt_curvature = curvature;
-	if(u_kmax > u_kmin)
-		gt_curvature = (curvature-u_kmin) / (u_kmax-u_kmin);
-
-	if ((gt_curvature<0) || (gt_curvature>1)) color= vec3(0.5,0.5,0.5);
-	else
-		color= colormap(gt_curvature);
-	
-	vertex_color = color;
+	curv_value = curvature;
 	curv_dir_max = vec3(0, 0, 1);
 	curv_dir_min = vec3(1, 0, 1);
 	
@@ -101,12 +58,14 @@ in vec3 vertex_position[];
 in vec3 vertex_color[];
 in vec3 curv_dir_max[];
 in vec3 curv_dir_min[];
+in float curv_value[];
 
 out vec3 geometry_position;
 out vec3 geometry_normal;
 out vec3 geometry_view;
 out vec3 geometry_distance;
 out vec3 geometry_color;
+out float geometry_curv_value;
 out flat int geometry_curvdir;
 
 void setPoint(vec3 point, vec3 color)
@@ -147,6 +106,7 @@ void main()
 		vec3 center = (pts_abs[1] + pts_abs[2] + pts_abs[0]) / 3;
 		gl_PrimitiveID = int( length(center)*1000 );
 		geometry_color = vertex_color[i];
+		geometry_curv_value = curv_value[i];
 		EmitVertex();
 	}
 	
@@ -296,21 +256,78 @@ layout(early_fragment_tests) in;
 in vec3 geometry_position;
 in vec3 geometry_distance;
 in vec3 geometry_view;
+in float geometry_curv_value;
 in vec3 geometry_color;
 in flat int geometry_curvdir;
 
+uniform float u_kmin;
+uniform float u_kmax;
 uniform int solid_wireframe;
 uniform vec3 u_camera_pos;
 
 out vec4 fragment_color;
 
+vec3 HSVtoRGB(vec3 hsv)
+{
+  int i;
+  double f, p, q, t;
+  if( hsv.y == 0 ) {                     // achromatic (gray)
+    return vec3(hsv.z);
+  }
+  i = int( floor( hsv.x / 60 ) );
+  f = ( hsv.x / 60 ) - i;                        // factorial part of h
+  p = hsv.z * ( 1.0 - hsv.y );
+  q = hsv.z * ( 1.0 - hsv.y * f );
+  t = hsv.z * ( 1.0 - hsv.y * ( 1.0 - f ) );
+
+  if (i==0)
+  	return vec3(hsv.z, t, p);
+  if (i==1)
+  	return vec3(q, hsv.z, p);
+  if (i==2)
+  	return vec3(p, hsv.z, t);
+  if (i==3)
+  	return vec3(p, q, hsv.z);
+  if (i==4)
+  	return vec3(t, p, hsv.z);
+
+  return vec3(hsv.z, p, q);
+}
+
+vec3 colormap(float scale)
+{
+  float cycles = 1;
+  const double hue = 360 * ( scale * cycles - floor(scale * cycles));
+  return HSVtoRGB( vec3(hue, 0.9, 1.0) );
+}
+
+vec3 colorFromCurv(float c)
+{
+	vec3 color;
+	float gt_curvature = c;
+	if(u_kmax > u_kmin)
+		gt_curvature = (c-u_kmin) / (u_kmax-u_kmin);
+
+	if ((gt_curvature<0) || (gt_curvature>1)) color= vec3(0.5,0.5,0.5);
+	else
+		color= colormap(gt_curvature);
+	
+	return color;
+}
+
 void main( )
 {
 	vec3 geometry_normal = -1 * normalize(cross( dFdx(geometry_position.xyz), dFdy(geometry_position.xyz)));
+	vec3 color;
+	if (geometry_curvdir == 0)
+		color = colorFromCurv(geometry_curv_value);
+	else
+		color = geometry_color;
+	
 	//Phong
 	float shadow_weight = 0.5;
 	float dotnormal = abs(dot(normalize(geometry_normal), vec3(-1, -1, -1)));
-	fragment_color = vec4( shadow_weight * geometry_color * dotnormal + (1-shadow_weight) * geometry_color, 1);
+	fragment_color = vec4( shadow_weight * color * dotnormal + (1-shadow_weight) * color, 1);
 
 	if (geometry_curvdir == 0 && solid_wireframe == 1)
 	{
